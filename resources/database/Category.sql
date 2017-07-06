9d15ab25e84adb843d43dea7112c25e3983769f4
Dinh Dang <dinhdq188@gmail.com>
2017-07-07 00:16:56 +0700

    [java-web-example] Upper: car back end.

--- resources/database/Category.sql
+++ resources/database/Category.sql
@@ -1,8 +1,13 @@
-CREATE TABLE Category (
+CREATE TABLE Categories (
     CategoryID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Name nvarchar(255) NOT NULL,
     Summary varchar(4000)
 );
-INSERT INTO Category (Name, Summary) VALUE('Mercedes', 'This is Mercedes');
-INSERT INTO Category (Name, Summary) VALUE('BMW', 'This is BMW');
-INSERT INTO Category (Name, Summary) VALUE('Audi', 'This is Audi');
+
+INSERT INTO Categories (Name, Summary) VALUE('Mercedes', 'This is Mercedes');
+INSERT INTO Categories (Name, Summary) VALUE('BMW', 'This is BMW');
+INSERT INTO Categories (Name, Summary) VALUE('Audi', 'This is Audi');
+INSERT INTO Categories (Name, Summary) VALUE('Mini Cooper', 'This is Mini Cooper');
+INSERT INTO Categories (Name, Summary) VALUE('Lexus', 'This is Lexus');
+INSERT INTO Categories (Name, Summary) VALUE('Mini Cooper', 'This is Mini Cooper');
+INSERT INTO Categories (Name, Summary) VALUE('Porsche', 'This is Porsche');
--- src/web/example/com/services/upper/ICar.java
+++ src/web/example/com/services/upper/ICar.java
@@ -1,7 +1,13 @@
 package web.example.com.services.upper;
 
+import web.example.com.entity.Cars;
+
+import java.util.List;
+
 /**
  * Created by datdq295 on 7/7/17.
  */
 public interface ICar {
+
+    List<Cars> getAllCars();
 }
